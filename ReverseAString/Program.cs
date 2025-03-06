fcc3595af3227069de012cb1ee6858e061114beb
vismayrathore <vismayrathore2021@gmail.com>
2025-03-06 10:18:15 +0530

    added reverse string

---
+++ ReverseAString/Program.cs
@@ -0,0 +1,37 @@
+﻿//You are given a string s, and your task is to reverse the string.
+
+//Examples:
+
+//Input: s = "Geeks"
+//Output: "skeeG"
+//Input: s = "for"
+//Output: "rof"
+//Input: s = "a"
+//Output: "a"
+
+using System;
+using System.Security.Policy;
+
+namespace ReverseAString
+{
+    public class Program
+    {
+        public static string reverseString(string str)
+        {
+            string rev = "";
+            for(int i = str.Length - 1;i>=0; i--)
+            {
+                rev += str[i];
+
+            }
+            return rev;
+        }
+        static void Main(string[] args)
+        {
+            string str = "Geeks";
+            string result = Program.reverseString(str);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
